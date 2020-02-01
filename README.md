d31b07a706aacda2e13e95e4f01b6b1afbbccb86
unknown <sondrelg@live.no>
2020-02-01 16:57:13 +0100

    Fixed tests

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-"# rscase" 
+![Py](https://img.shields.io/badge/Python-v3.8-blue.svg)
+![Py](https://img.shields.io/badge/Rust-v1.41.0-orange.svg)
+
+# rsCase
+
+rsCase is a simple python package implemented in [Rust](https://www.rust-lang.org/learn), using [pyo3](https://github.com/PyO3/pyo3) to access Rust binding for the python interpreter. 
+
+Package provides utility functions for the case-conversions:
+
+- Camel case
+- Snake case
+- Pascal case
+- Kebab case
+
+## Install 
+
+```shell
+pip install rscase
+```
+
+## Usage
+
+```python
+from rscase import camel_case
+
+camel_case('this_is-a_Test')
+>> thisIsATest
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-#[macro_use]
 extern crate pyo3;
 
 use std::str;
 use std::string::String;
 
-use pyo3::{PyResult, wrap_pyfunction};
 use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
 
 #[pyfunction]
 fn snake_case(s: &str) -> PyResult<String> {
@@ -109,7 +108,15 @@ fn kebab_case(s: &str) -> PyResult<String> {
 }
 
 
+#[pyfunction]
+fn train_case(s: &str) -> PyResult<String> {
+    let cased_s = kebab_case(&s);
+    Ok(cased_s.unwrap().to_uppercase())
+}
+
+
 #[pymodule]
+#[allow(unused_variables)]
 fn rscase(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(camel_case))?;
     m.add_wrapped(wrap_pyfunction!(snake_case))?;
@@ -125,72 +132,72 @@ mod tests {
 
     #[test]
     fn valid_snake_case_translations() {
-        assert_eq!(snake_case("PascalCase"), "pascal_case");
-        assert_eq!(snake_case("camelCase"), "camel_case");
-        assert_eq!(snake_case("snake_case"), "snake_case");
-        assert_eq!(snake_case("kebab-case"), "kebab_case");
-        assert_eq!(snake_case("TRAIN-CASE"), "t_r_a_i_n__c_a_s_e");
-        assert_eq!(snake_case(""), "");
-        assert_eq!(snake_case("1"), "1");
-        assert_eq!(snake_case("-22"), "-22");
-        assert_eq!(snake_case("lower"), "lower");
-        assert_eq!(snake_case("UPPER"), "u_p_p_e_r");
+        assert_eq!(snake_case("PascalCase").unwrap(), "pascal_case");
+        assert_eq!(snake_case("camelCase").unwrap(), "camel_case");
+        assert_eq!(snake_case("snake_case").unwrap(), "snake_case");
+        assert_eq!(snake_case("kebab-case").unwrap(), "kebab_case");
+        assert_eq!(snake_case("TRAIN-CASE").unwrap(), "t_r_a_i_n__c_a_s_e");
+        assert_eq!(snake_case("").unwrap(), "");
+        assert_eq!(snake_case("1").unwrap(), "1");
+        assert_eq!(snake_case("-22").unwrap(), "-22");
+        assert_eq!(snake_case("lower").unwrap(), "lower");
+        assert_eq!(snake_case("UPPER").unwrap(), "u_p_p_e_r");
     }
 
     #[test]
     fn valid_camel_case_translations() {
-        assert_eq!(camel_case("PascalCase"), "pascalCase");
-        assert_eq!(camel_case("camelCase"), "camelCase");
-        assert_eq!(camel_case("snake_case"), "snakeCase");
-        assert_eq!(camel_case("kebab-case"), "kebabCase");
-        assert_eq!(camel_case("TRAIN-CASE"), "tRAINCASE");
-        assert_eq!(camel_case(""), "");
-        assert_eq!(camel_case("1"), "1");
-        assert_eq!(camel_case("-22"), "-22");
-        assert_eq!(camel_case("lower"), "lower");
-        assert_eq!(camel_case("UPPER"), "uPPER");
+        assert_eq!(camel_case("PascalCase").unwrap(), "pascalCase");
+        assert_eq!(camel_case("camelCase").unwrap(), "camelCase");
+        assert_eq!(camel_case("snake_case").unwrap(), "snakeCase");
+        assert_eq!(camel_case("kebab-case").unwrap(), "kebabCase");
+        assert_eq!(camel_case("TRAIN-CASE").unwrap(), "tRAINCASE");
+        assert_eq!(camel_case("").unwrap(), "");
+        assert_eq!(camel_case("1").unwrap(), "1");
+        assert_eq!(camel_case("-22").unwrap(), "-22");
+        assert_eq!(camel_case("lower").unwrap(), "lower");
+        assert_eq!(camel_case("UPPER").unwrap(), "uPPER");
     }
 
     #[test]
     fn valid_pascal_case_translations() {
-        assert_eq!(pascal_case("PascalCase"), "PascalCase");
-        assert_eq!(pascal_case("camelCase"), "CamelCase");
-        assert_eq!(pascal_case("snake_case"), "SnakeCase");
-        assert_eq!(pascal_case("kebab-case"), "KebabCase");
-        assert_eq!(pascal_case("TRAIN-CASE"), "TRAINCASE");
-        assert_eq!(pascal_case(""), "");
-        assert_eq!(pascal_case("1"), "1");
-        assert_eq!(pascal_case("-22"), "-22");
-        assert_eq!(pascal_case("lower"), "Lower");
-        assert_eq!(pascal_case("UPPER"), "UPPER");
+        assert_eq!(pascal_case("PascalCase").unwrap(), "PascalCase");
+        assert_eq!(pascal_case("camelCase").unwrap(), "CamelCase");
+        assert_eq!(pascal_case("snake_case").unwrap(), "SnakeCase");
+        assert_eq!(pascal_case("kebab-case").unwrap(), "KebabCase");
+        assert_eq!(pascal_case("TRAIN-CASE").unwrap(), "TRAINCASE");
+        assert_eq!(pascal_case("").unwrap(), "");
+        assert_eq!(pascal_case("1").unwrap(), "1");
+        assert_eq!(pascal_case("-22").unwrap(), "-22");
+        assert_eq!(pascal_case("lower").unwrap(), "Lower");
+        assert_eq!(pascal_case("UPPER").unwrap(), "UPPER");
     }
 
 
     #[test]
     fn valid_kebab_case_translations() {
-        assert_eq!(kebab_case("PascalCase"), "pascal-case");
-        assert_eq!(kebab_case("camelCase"), "camel-case");
-        assert_eq!(kebab_case("snake_case"), "snake-case");
-        assert_eq!(kebab_case("kebab-case"), "kebab-case");
-        assert_eq!(kebab_case("TRAIN-CASE"), "t-r-a-i-n--c-a-s-e");
-        assert_eq!(kebab_case(""), "");
-        assert_eq!(kebab_case("1"), "1");
-        assert_eq!(kebab_case("-22"), "-22");
-        assert_eq!(kebab_case("lower"), "lower");
-        assert_eq!(kebab_case("UPPER"), "u-p-p-e-r");
+        assert_eq!(kebab_case("PascalCase").unwrap(), "pascal-case");
+        assert_eq!(kebab_case("camelCase").unwrap(), "camel-case");
+        assert_eq!(kebab_case("snake_case").unwrap(), "snake-case");
+        assert_eq!(kebab_case("kebab-case").unwrap(), "kebab-case");
+        assert_eq!(kebab_case("TRAIN-CASE").unwrap(), "t-r-a-i-n--c-a-s-e");
+        assert_eq!(kebab_case("").unwrap(), "");
+        assert_eq!(kebab_case("1").unwrap(), "1");
+        assert_eq!(kebab_case("-22").unwrap(), "-22");
+        assert_eq!(kebab_case("lower").unwrap(), "lower");
+        assert_eq!(kebab_case("UPPER").unwrap(), "u-p-p-e-r");
     }
 
     #[test]
     fn valid_train_case_translations() {
-        assert_eq!(train_case("PascalCase"), "PASCAL-CASE");
-        assert_eq!(train_case("camelCase"), "CAMEL-CASE");
-        assert_eq!(train_case("snake_case"), "SNAKE-CASE");
-        assert_eq!(train_case("kebab-case"), "KEBAB-CASE");
-        assert_eq!(train_case("TRAIN-CASE"), "T-R-A-I-N--C-A-S-E");
-        assert_eq!(train_case(""), "");
-        assert_eq!(train_case("1"), "1");
-        assert_eq!(train_case("-22"), "-22");
-        assert_eq!(train_case("lower"), "LOWER");
-        assert_eq!(train_case("UPPER"), "U-P-P-E-R");
+        assert_eq!(train_case("PascalCase").unwrap(), "PASCAL-CASE");
+        assert_eq!(train_case("camelCase").unwrap(), "CAMEL-CASE");
+        assert_eq!(train_case("snake_case").unwrap(), "SNAKE-CASE");
+        assert_eq!(train_case("kebab-case").unwrap(), "KEBAB-CASE");
+        assert_eq!(train_case("TRAIN-CASE").unwrap(), "T-R-A-I-N--C-A-S-E");
+        assert_eq!(train_case("").unwrap(), "");
+        assert_eq!(train_case("1").unwrap(), "1");
+        assert_eq!(train_case("-22").unwrap(), "-22");
+        assert_eq!(train_case("lower").unwrap(), "LOWER");
+        assert_eq!(train_case("UPPER").unwrap(), "U-P-P-E-R");
     }
 }
