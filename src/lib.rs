c737b9ec1acd8260108fb692df8c980ad3fddcfb
unknown <sondrelg@live.no>
2020-02-02 11:12:19 +0100

    ci bug

--- src/lib.rs
+++ src/lib.rs
@@ -131,7 +131,6 @@ pub fn train_case(s: &str) -> PyResult<String> {
 
 #[pymodule]
 #[allow(unused_variables)]
-#[cfg_attr(tarpaulin, skip)]
 fn cases(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(camel_case))?;
     m.add_wrapped(wrap_pyfunction!(snake_case))?;
