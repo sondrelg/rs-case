10f781765adf3ad7ff0124d05d3a3833a6bb0869
unknown <sondrelg@live.no>
2020-02-18 21:50:03 +0100

    Trying to build with actions

---
+++ build/lib/rscase/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+rsCase is a simple python package implemented in Rust for string case translation.
+"""
+
+__author__ = "Sondre Lillebø Gundersen"
+__version__ = "1.1.0"  # Remember to change cargo.toml
